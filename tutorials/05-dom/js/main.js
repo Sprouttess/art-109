82dc389f6738b0c3490e0f0ae07407be12c5d001
Sprouttess <kobryan007@gmail.com>
2025-04-15 17:13:08 -0700

    05-dom

---
+++ tutorials/05-dom/js/main.js
@@ -0,0 +1,13 @@
+
+document.querySelector("#bread-3").addEventListener("click", function(){
+    document.querySelector("#bread-2").style.visibility = "visible";
+    alert(LOAF);
+})
+
+document.querySelector("#bread-2").addEventListener("click", function(){
+    document.querySelector("#bread-music").style.visibility = "visible";
+})
+
+document.querySelector("#bread-music").addEventListener("click", function(){
+ 
+})
