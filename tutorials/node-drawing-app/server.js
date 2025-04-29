1618381d1c10f2baa99777f7c776b8d0e4506df3
Sprouttess <kobryan007@gmail.com>
2025-04-29 03:28:18 -0700

    tut 9

---
+++ tutorials/node-drawing-app/server.js
@@ -0,0 +1,29 @@
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("hello world");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection: ' + socket.id);
+
+    socket.on('mouse', nouseMsg);
+
+    function mouseMsg(data) {
+        socket.brodcast.emit('mouse', data);
+        //io.sockets.emit('mouse', data);
+        console.log(data);
+    }
+
+
+}
